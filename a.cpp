525adbd1a3438c503233c2a6dad3deeec06dea5a
TulebaevTemirlan <47266010+TulebaevTemirlan@users.noreply.github.com>
2020-03-12 17:33:16 +0600

    Euclidean algorithm

---
+++ a.cpp
@@ -0,0 +1,23 @@
+// gcd function by Euclidean algorithm
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    while(a > 0 && b > 0){
+        if(a > 0){
+            a %= b;
+        }
+        else{
+            b %= a;
+        }
+    }
+    return a+b;
+}
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << gcd(a, b);
+}
